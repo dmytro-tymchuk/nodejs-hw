1b0c8e2520293a580c6f75d19221e6ab44d0dbd6
Dmytro Tymchuk <d.tym@ukr.net>
2025-10-31 16:23:42 -0500

    final updates

--- src/server.js
+++ src/server.js
@@ -1,2 +1,37 @@
-const a = 0;
-console.log(a);
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+  res.status(500).json({ message: isProd ? 'Oops, some error' : err.message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is runign on port ${PORT}`);
+});
