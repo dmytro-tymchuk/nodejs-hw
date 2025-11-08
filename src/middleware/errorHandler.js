4cc121aa91fe38c15d8153f318701c5215854ffc
Dmytro Tymchuk <d.tym@ukr.net>
2025-11-08 13:57:07 -0600

    final changes

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,12 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
   const isProd = process.env.NODE_ENV === 'production';
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: isProd ? 'Oops, some error' : err.message || err.name,
+    });
+  }
+
   res.status(500).json({ message: isProd ? 'Oops, some error' : err.message });
 };
