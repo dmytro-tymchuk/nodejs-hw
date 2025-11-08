b1dc1edb5174f6667d3d99e2dfc13fb7ecf8e372
Dmytro Tymchuk <d.tym@ukr.net>
2025-11-08 11:41:19 -0600

    updates

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,4 @@
+export const errorHandler = (err, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+  res.status(500).json({ message: isProd ? 'Oops, some error' : err.message });
+};
--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import pino from 'pino-http';
 import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -10,44 +14,14 @@ const PORT = process.env.PORT ?? 3000;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger);
 
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
+app.use(notesRoutes);
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-app.use((err, req, res, next) => {
-  const isProd = process.env.NODE_ENV === 'production';
-  res.status(500).json({ message: isProd ? 'Oops, some error' : err.message });
-});
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is runign on port ${PORT}`);
